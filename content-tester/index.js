f111f20ef5d991b990909bbb5daf1c1f43bcc11f
Adam Plouff <adamplouff@gmail.com>
2023-03-21 14:17:55 -0400

    Remove tester

--- content-tester/index.js
+++
@@ -1,9 +0,0 @@
-let currentProduct = 'Rubberhose3'
-let products = ['Overlord', 'Anubis', 'Rubberhose3']
-
-fetch("../index.json")
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("content").innerHTML = data[currentProduct].en; 
-    console.log(data)
-  })
